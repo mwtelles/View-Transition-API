9e6be733dd5e9b0972405b2098447068d36f781a
Matheus Telles <business.mwtelles@gmail.com>
2025-02-06 00:36:58 -0300

    add: animations using gsap

--- transition.js
+++ transition.js
@@ -1,3 +1,86 @@
+let lenis;
+
+function initializeLenis() {
+    if (lenis) {
+        lenis.destroy();
+    }
+
+    lenis = new Lenis({
+        autoRaf: true,
+        smoothWheel: true,
+    });
+
+    function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+}
+
+function initializeAnimations() {
+    gsap.to('.link a', {
+        y: 0,
+        duration: 1,
+        stagger: 0.1,
+        ease: 'power4.out',
+        delay: 1,
+    });
+
+    if (document.querySelector('.hero h1')) {
+        const heroText = new SplitType('.hero h1', {
+            types: 'chars'
+        });
+        gsap.set(heroText.chars, {
+            y: 400,
+        });
+        gsap.to(heroText.chars, {
+            y: 0,
+            duration: 1,
+            stagger: 0.075,
+            ease: 'power4.out',
+            delay: 1,
+        });
+    }
+
+    if (document.querySelector('.info p')) {
+        const splits = document.querySelectorAll('.info p .line');
+        splits.forEach((split) => {
+            const text = split.textContent;
+            split.parentNode.replaceChild(document.createTextNode(text), split);
+        });
+
+        const text = new SplitType('.info p', {
+            types: 'lines',
+            tagName: 'div',
+            lineClass: 'line',
+        });
+
+        text.lines.forEach((line) => {
+            const content = line.innerHTML;
+            line.innerHTML = `<span>${content}</span>`;
+        });
+
+        gsap.set('.info p .line span', {
+            y: 400,
+            display: 'block',
+        });
+
+        gsap.to('.info p .line span', {
+            y: 0,
+            duration: 2,
+            stagger: 0.075,
+            ease: 'power4.out',
+            delay: 0.25,
+        })
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    initializeLenis();
+    initializeAnimations();
+});
+
 if (navigation.addEventListener) {
     navigation.addEventListener('navigate', (event) => {
         if (!event.destination.url.includes(document.location.origin)) {
@@ -19,6 +102,8 @@ if (navigation.addEventListener) {
 
                 transition.ready.then(() => {
                     window.scrollTo(0, 0);
+                    initializeAnimations();
+                    initializeLenis();
                 });
             },
             scroll: "manual",
